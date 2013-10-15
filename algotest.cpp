d28ec3531e57a93278484fc8b0207fb0dabd5215
Afanc <dariush.mollet@epfl.ch>
2013-10-15 13:46:34 +0200

    algotest almost !

--- algotest.cpp
+++ algotest.cpp
@@ -36,30 +36,41 @@ ostream& operator<<(ostream& sortie, const Alignement & a)
 //void f(Alignement a, string s1, string s2, int index1, int index2)
 void f(Alignement a, string s1, string s2, int index1, int index2, int maxscore=0, vector<Alignement> resultat = {0})
 	{
-		if(index1 != s1.size() && index2 != s2.size())
+		cout<<"i1 = "<<index1<<" i2 = "<<index2<<endl;
+		while((index1 != index2) || (index1 <= 2*s1.size()) || (index2 <= s2.size()))
 		{
-			if (index1 != s1.size()) //suppress
+			if (index1 <= s1.size()) //suppress
 			{	
+				cout<<"suppress !"<<endl;
+				cout<<"a before = "<<endl<<a<<endl;
 				a.addback('-',s2[index2]);
 				++index2;
-				f(a,s1,s2,index1+1,index2,maxscore, resultat);
+				cout<<"a after = "<<endl<<a<<endl;
+				f(a,s1,s2,index1,index2,maxscore, resultat);
 			}	
-			if (index2 != s2.size()) //insert
+			if (index2 <= s2.size()) //insert
 			{
+				cout<<"insert !"<<endl;
+				cout<<"a before = "<<endl<<a<<endl;
 				a.addback(s1[index1], '-');
 				++index1;
+				cout<<"a after = "<<endl<<a<<endl;
 				f(a,s1,s2,index1,index2, maxscore, resultat);	
 			}
-			if(index1 != s1.size() && index2 != s2.size()) //substitute
+			if(index1 <= s1.size() && index2 <= s2.size()) //substitute
 			{	
+				cout<<"substitute !"<<endl;
+				cout<<"a before = "<<endl<<a<<endl;
 				a.addback(s1[index1],s2[index2]);
 				++index1;
 				++index2;
+				cout<<"a after = "<<endl<<a<<endl;
 				f(a,s1,s2,index1,index2, maxscore, resultat);
 			}
-		
+		cout<<"-----"<<endl;
 		}
-		else{
+		if(index1 == s1.size() == index2 == s2.size())
+		{
 			int x = a.getScore();
 			if(x > maxscore)
 			{
@@ -71,14 +82,15 @@ void f(Alignement a, string s1, string s2, int index1, int index2, int maxscore=
 				resultat.push_back(a);
 			}
 		
-			cout<<a<<endl;
+			cout<<a<<endl<<"_______________"<<endl;
 			cout<<endl;
 		}
 	}
 
 int main() {
-	string a("AA");
-	string b("AA");
+	string a("AC");
+	string b("GA");
+	cout<<a<< " vs "<<b<<endl;
 	Alignement A;
 	vector<Alignement> x;
 	f(A,a,b,0,0,0,x);
